1c709049015aeb7e9abb904d163a2209d79d741e
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-09-08 21:30:17 +0300

    set up stepper

--- TipsAppV2/ViewController.swift
+++ TipsAppV2/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var friendsCountLAbel: UILabel!
+    @IBOutlet var stepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        friendsCountLAbel.text = "0"
     }
 
 
+    @IBAction func tipButtonPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stepperPressed(_ sender: Any) {
+        friendsCountLAbel.text = String(format: "%.0f", stepper.value)
+    }
 }
 
